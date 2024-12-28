57080f62aeb7806f9f19100651856b5a341247c4
Chris Gray <chgray@microsoft.com>
2024-12-28 13:26:15 -0700

    working on binding tools

--- docs/docs/bind.ps1
+++ docs/docs/bind.ps1
@@ -4,7 +4,7 @@ $DB_DIR = Join-Path $PWD "..\orig_media"
 $DB_DIR = Resolve-Path $DB_DIR
 $env:CDOCS_DB = $DB_DIR
 
-$md = (Get-Content ../../mkdocs.yml | Select-String ".md")
+$md = (Get-Content ../../mkdocs.yml)
 #$md = (type ../../mkdocs.yml | grep ".md")
 
 
@@ -21,29 +21,39 @@ if (Test-Path "..\bound_docs\bind.files")
 
 try {
 	cd ..
+	$inNav = $false
 	foreach($file in $md)
 	{
 		$file = $file.ToString()
 
-		if(!$file.EndsWith(".md"))
+		if($file -eq "#<START_BINDING>")
 		{
-			Write-Host ("Skipping : $file")
+			Write-Host "Found NAV"
+			$inNav = $true
+			continue
+		}
+		elseif($file -eq "#<END_BINDING>")
+		{
+			Write-Host "End NAV"
+			$inNav = $false
 			continue
 		}
 
-		$seperator = $file.IndexOf("-")
-		$file = $file.ToString().Trim()
-		$file=$file.ToString().Trim().Split(":")[1].ToString().Trim()
-
-		if(!(Test-Path $file))
+		if (!$inNav)
 		{
-			Write-Error "file doesnt exist : $file"
-			exit 5
+			continue
 		}
 
-		$file_leaf = Split-Path -Path $file -Leaf
+		if($file.Trim().Length -eq 0)
+		{
+			continue
+		}
 
+
+		$seperator = $file.IndexOf("-")
 		$env:CDOCS_TAB=0
+		$env:CDOCS_FILTER=1
+
 		if ($seperator -gt 4)
 		{
 			$seperatorx = $seperator
@@ -52,7 +62,33 @@ try {
 			$env:CDOCS_TAB = $seperatorx
 		}
 
-		Write-Host "Convertinging $file"
+
+
+		if(!$file.EndsWith(".md"))
+		{
+			Write-Host ("Generating : [$file]")
+			$me = $file.Replace("-","").Replace(":","").Trim();
+			$id = New-Guid
+			Write-Host (" ==> $me ==> $id")
+
+			$file = ".\bound_docs\$id.generated.md"
+			$file_leaf = "$id.generated.converted.md"
+
+			Add-Content "$file" "# $me"
+		} else {
+			$file = $file.ToString().Trim()
+			$file=$file.ToString().Trim().Split(":")[1].ToString().Trim()
+
+			if(!(Test-Path $file))
+			{
+				Write-Error "file doesnt exist : $file"
+				exit 5
+			}
+
+			$file_leaf = Split-Path -Path $file -Leaf
+		}
+
+		Write-Host "Convertinging $file == > $file_leaf"
 		pandoc -i $file -o ./bound_docs/$file_leaf --filter CDocsMarkdownCommentRender
 
 		Add-Content ".\bound_docs\bind.files" $file_leaf
