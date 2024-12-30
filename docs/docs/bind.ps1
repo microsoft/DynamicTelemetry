cb2849cf68d0dd98d1c8a29b38f94d938a915cf2
Chris Gray <chgray@microsoft.com>
2024-12-29 18:37:18 -0700

    formatting

--- docs/docs/bind.ps1
+++ docs/docs/bind.ps1
@@ -4,6 +4,9 @@ $DB_DIR = Join-Path $PWD "..\orig_media"
 $DB_DIR = Resolve-Path $DB_DIR
 $env:CDOCS_DB = $DB_DIR
 
+
+$env:PATH+=";C:\\Source\\CDocs\\tools\\CDocsMarkdownCommentRender\\bin\\Debug\\net8.0"
+
 $md = (Get-Content ../../mkdocs.yml)
 #$md = (type ../../mkdocs.yml | grep ".md")
 
