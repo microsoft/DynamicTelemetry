f87c2e77d842de95e3c872303c6355962dc1d0c4
Chris Gray <chgray@microsoft.com>
2025-01-08 14:19:36 -0800

    working on sample

--- docs/Samples/Demo1-Introduction/DynamicTelemetry-Demo1-Introduction/Pages/Index.cshtml.cs
+++ docs/Samples/Demo1-Introduction/DynamicTelemetry-Demo1-Introduction/Pages/Index.cshtml.cs
@@ -1,20 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Diagnostics.Metrics;
 
 namespace DynamicTelemetry_Demo1_Introduction.Pages
 {
-    public class IndexModel : PageModel
+    public partial class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private Counter<int> _getCounter;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IMeterFactory meterFactory)
         {
+            var meter = meterFactory.Create("DynamicTelemetry.Sample1.IndexModel");
+            _getCounter = meter.CreateCounter<int>("OnGet");
             _logger = logger;
         }
 
         public void OnGet()
         {
+            _getCounter.Add(1);
 
+            string url = HttpContext.Request.GetDisplayUrl();
+            LogGet(_logger, url);
         }
+
+        [LoggerMessage(Level = LogLevel.Information, Message = "[GENERATED.3] -- Log Get({url})")]
+        static partial void LogGet(ILogger logger, string url);
     }
 }
