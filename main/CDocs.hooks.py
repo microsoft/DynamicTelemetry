9429454e4ba987291565d82d5975f6c5ef0eed4c
Chris Gray <chgray@gmail.com>
2025-02-05 09:55:17 -0800

    extended character removal

--- main/CDocs.hooks.py
+++ main/CDocs.hooks.py
@@ -10,6 +10,18 @@ import mkdocs.plugins
 import CDocs_utils as CDocs
 from sanitycheck_helpers import sanitycheck
 
+def ends_with_newline(file_path):
+    with open(file_path, 'rb') as file:
+        file.seek(-1, 2)  # Move the cursor to the last byte of the file
+        last_char = file.read(1)
+        return last_char == b'\n'
+
+def filter_bytes(file_path):
+    with open(file_path, 'rb') as file:
+        data = file.read()
+    filtered_data = bytes(byte for byte in data if byte <= 127)
+
+    return filtered_data
 
 @mkdocs.plugins.event_priority(50000)
 def on_page_markdown(markdown: str, page: Page, config: MkDocsConfig, **kwargs) -> str | None:
@@ -22,13 +34,15 @@ def on_page_markdown(markdown: str, page: Page, config: MkDocsConfig, **kwargs) 
 
     file = "./docs/" + page.file.src_path
     if 0 != sanitycheck(file):
+        #
+        # BUGBUG : keep this file rewrite here until we're sure where these
+        #          extended characters are coming from (pandoc)
+        #
+        #filtered_data = filter_bytes(file)
+        #with open(file, 'wb') as out_file:
+        #    out_file.write(filtered_data)
         raise Exception("Sanity Check failed for " + file)
 
-    #for name, value in os.environ.items():
-    #    markdown += "{0}: {1}\n".format(name, value)
-
-    #for property, value in vars(page).items():
-    #    markdown += "{0}: {1}\n".format(property, value)
 
     data = ""
     if -1 == markdown.find("ProvideFeedback"):
