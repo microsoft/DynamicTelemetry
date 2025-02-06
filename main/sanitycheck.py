84c375e29c94874b932270d386f9a1e83debf742
Reiley Yang <reyang@microsoft.com>
2025-02-06 14:43:50 -0800

    Update main/sanitycheck.py

--- main/sanitycheck.py
+++ main/sanitycheck.py
@@ -13,7 +13,7 @@ LF = b'\n'
 retval = 0
 retval += sanitycheck('.github/**/*.md', allow_eol = (LF,))
 retval += sanitycheck('.github/**/*.yml', allow_eol = (LF,), indent = 2)
-retval += sanitycheck('**/*.cs', allow_eol = (LF,), indent = 2)
+retval += sanitycheck('**/*.cs', allow_eol = (LF,), indent = 4)
 retval += sanitycheck('**/*.css', allow_eol = (LF,), indent = 2)
 retval += sanitycheck('**/*.htm', allow_eol = (LF,), indent = 4)
 retval += sanitycheck('**/*.html', allow_eol = (LF,), indent = 4)
