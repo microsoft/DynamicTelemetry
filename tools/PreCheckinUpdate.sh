96cfade8b29f9e4e94df9fcf9e00316d7384a55a
Chris Gray <chgray@microsoft.com>
2025-02-18 13:34:28 -0800

    produce epub, pdf, and docx

--- tools/PreCheckinUpdate.sh
+++ tools/PreCheckinUpdate.sh
@@ -16,10 +16,18 @@ cd /data/docs/bound_docs
 dos2unix ./bind.files
 pandoc -i $(cat ./bind.files) -o ./_bound.tmp.md
 
+fileName=$(date +DynamicTelemetry-Draft-%b-%d-%H-%M-%S)
+
+echo "---" > ./title.txt
+echo "Title:$fileName" >> ./title.txt
+echo "Author:Chris Gray at al" >> ./title.txt
+echo "---" >> ./title.txt
+
+
 cat ./title.txt ./_bound.tmp.md | grep -v mp4 > ./bound.md
 
-pandoc ./bound.md -o ./bound.epub
-pandoc ./bound.md -o ./bound.pdf
-pandoc ./bound.md -o ./bound.docx
+pandoc ./bound.md -o ./$fileName.epub
+pandoc ./bound.md -o ./$fileName.pdf
+pandoc ./bound.md -o ./$fileName.docx
 
 ls -l ./bound*
