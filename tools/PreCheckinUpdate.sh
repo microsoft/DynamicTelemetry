a2cdd914a3c2c88019fbbfdcc8126b1e1a8cce66
Chris Gray <chgray@microsoft.com>
2025-03-02 17:09:37 -0800

    working on container

--- tools/PreCheckinUpdate.sh
+++ tools/PreCheckinUpdate.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 #
+# See if CDocs has been built; if not sync and build
+#
+if [ ! -d "/Source/CDocs" ]; then
+    git clone https://github.com/chgray/CDocs /Source/CDocs
+    cd /Source/CDocs
+    git checkout user/chgray/update_ubuntu
+    cd /Source/CDocs/tools/CDocsMarkdownCommentRender
+    dotnet build .
+fi
+
+if [ ! -f "/out/.CDocs.config" ]; then
+    touch /out/.CDocs.config
+fi
+
+#
 # READ-WRITE Update Status and Probes
 #
 cd /data_rw/docs/docs
@@ -38,9 +53,14 @@ echo "---" >> ./title.txt
 cat ./title.txt ./_bound.tmp.md | grep -v mp4 > ./bound.md
 
 echo "Building bound contents; in docx, pdf, and epub"
-pandoc ./bound.md --toc --toc-depth 6 --epub-cover-image=../orig_media/DynamicTelemetry.CoPilot.Image.png -o /out/bound/epub_$fileName.epub
-pandoc ./bound.md -o /out/bound/$fileName.pdf  --toc --toc-depth 6 -N -V geometry:margin=0.25in -V papersize=a5
-pandoc ./bound.md -o /out/bound/$fileName.docx
+
+export CDOCS_FILTER=1
+export PATH=$PATH:/Source/CDocs/tools/CDocsMarkdownCommentRender/bin/Debug/net8.0
+
+pandoc ./bound.md --toc --toc-depth 6 --epub-cover-image=../orig_media/DynamicTelemetry.CoPilot.Image.png -o /out/bound/epub_$fileName.epub --filter CDocsMarkdownCommentRender
+pandoc ./bound.md -o /out/bound/$fileName.pdf  --toc --toc-depth 6 -N -V geometry:margin=0.25in -V papersize=a5 --filter CDocsMarkdownCommentRender
+pandoc ./bound.md -o /out/bound/$fileName.docx --filter CDocsMarkdownCommentRender
+
 cp ./bound.md /out/bound
 
 echo "Done!"
