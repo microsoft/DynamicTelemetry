f9ff0bf4c675f5eee7dc7b6ab324015bf58f6fb2
Chris Gray <chgray@microsoft.com>
2025-03-21 19:59:56 -0700

    edits to demo 0, durable ID, based on phone calls this morning

--- docs/Samples/DurableIds/Pages/Index.cshtml.cs
+++ docs/Samples/DurableIds/Pages/Index.cshtml.cs
@@ -12,7 +12,6 @@ namespace DynamicTelemetry_Demo_DurableIds.Pages
         private readonly ILogger<IndexModel> _logger;
         private static string _version = "0.0.4";
         private Guid _instanceID = Guid.NewGuid();
-        //private static Counter<int> ? _getCounter;
 
         public IndexModel(ILogger<IndexModel> logger, IMeterFactory meterFactory)
         {
@@ -25,6 +24,16 @@ namespace DynamicTelemetry_Demo_DurableIds.Pages
             //_getCounter.Add(1);
             LogWithDurableID();
             LogWithoutDurableID();
+
+
+            // START : LoopRandomGUID
+            // Generate some data that will be tough to aggregate
+            for (int i = 0; i < 1000; i++)
+            {
+                _logger.LogInformation($"Loop, random ID={Guid.NewGuid()}");
+                LogRandomGuid(_logger, Guid.NewGuid());
+            }
+            // END : LoopRandomGUID
         }
 
         // StartExample:NoDurableId
@@ -64,11 +73,15 @@ namespace DynamicTelemetry_Demo_DurableIds.Pages
             LogLaunch(_logger, _version, _instanceID);
         }
 
-        // StartSearchExample:LogLaunch
         [LoggerMessage(Level = LogLevel.Information, Message = "Launch, ver={version}, instantion={instantionID}")]
         static partial void LogLaunch(ILogger logger, string version, Guid instantionID);
-        // EndSearchExample:LogLaunch
         // EndExample:DurableId
+
+
+        // START_DEFINE : LogRandomGuid
+        [LoggerMessage(Level = LogLevel.Information, Message = "Loop, random ID={guid}")]
+        static partial void LogRandomGuid(ILogger logger, Guid guid);
+        // END_DEFINE : LogRandomGuid
     }
 }
 // EndExample:ContrastDurableID
